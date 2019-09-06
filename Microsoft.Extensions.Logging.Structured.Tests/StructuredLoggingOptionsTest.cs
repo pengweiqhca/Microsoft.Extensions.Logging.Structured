2c5c37de897da5e46cf56be318fb78cfa24929eb
彭伟 <pengwei@tuhu.cn>
2019-09-06 13:05:19 +0800

    Add Test

---
+++ Microsoft.Extensions.Logging.Structured.Tests/StructuredLoggingOptionsTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Extensions.Logging.Structured.Tests
+{
+    public class StructuredLoggingOptionsTest
+    {
+        [Fact]
+        public void Layout()
+        {
+            var options = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    {"Logging:Test:Layout:A", typeof(DateTimeLayout).FullName},
+                    {"Logging:Test:Layout:B", "Abc"},
+                })
+                .Build().GetSection("Logging:Test")
+                .Get<StructuredLoggingOptions>();
+
+            Assert.NotNull(options);
+            Assert.Equal(options.Layout.Count, options.Layouts.Count);
+            Assert.True(options.Layouts.TryGetValue("A", out var layout));
+            Assert.IsType<DateTimeLayout>(layout);
+            Assert.True(options.Layouts.TryGetValue("B", out layout));
+            Assert.IsType<ConstLayout>(layout);
+            Assert.Equal("Abc", ((ConstLayout)layout).Format(default));
+        }
+    }
+}
