4b143e2aabc8b10ecbd5202b0368c8481d86da51
彭伟 <pengwei@tuhu.cn>
2019-12-04 09:49:24 +0800

    Add demo

---
+++ MySqlDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Structured;
+using Microsoft.Extensions.Logging.Structured.Console;
+using MySql.Data.MySqlClient;
+using MySqlConnector.Logging;
+using Newtonsoft.Json;
+
+namespace MySqlDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var provider = new ServiceCollection()
+                .AddLogging(lb => lb.AddConsole(settings => settings.NullValueHandling = NullValueHandling.Ignore)
+                    .AddLayout(new DateTimeLayout(), new LogLevelLayout(), new RenderedMessageLayout(), new ExceptionLayout()))
+                .BuildServiceProvider();
+
+            MySqlConnectorLogManager.Provider = new MicrosoftExtensionsLoggingLoggerProvider(provider.GetRequiredService<ILoggerFactory>());
+
+            using var connection = new MySqlConnection("server=localhost");
+
+            connection.Open();
+        }
+    }
+}
