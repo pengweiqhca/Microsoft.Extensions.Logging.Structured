379ea2e5fc2e174f5625a5a9a8c27f719f263ba7
彭伟 <pengweiqhca@sina.com>
2021-10-11 20:24:06 +0800

    nullable

--- Microsoft.Extensions.Logging.Structured/LoggingEvent.cs
+++ Microsoft.Extensions.Logging.Structured/LoggingEvent.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Extensions.Logging.Structured
 {
     public readonly ref struct LoggingEvent
     {
-        private readonly Lazy<IEnumerable<object>> _scope;
+        private readonly Lazy<IEnumerable<object?>> _scope;
 
         public LoggingEvent(DateTimeOffset timeStamp, string categoryName, LogLevel logLevel,
             EventId eventId, object? message, string renderedMessage, Exception? exception, IExternalScopeProvider? scopeProvider)
@@ -18,11 +18,11 @@ namespace Microsoft.Extensions.Logging.Structured
             RenderedMessage = renderedMessage;
             TimeStamp = timeStamp;
 
-            if (scopeProvider == null) _scope = new Lazy<IEnumerable<object>>(Array.Empty<object>);
+            if (scopeProvider == null) _scope = new Lazy<IEnumerable<object?>>(Array.Empty<object?>);
             else
-                _scope = new Lazy<IEnumerable<object>>(() =>
+                _scope = new Lazy<IEnumerable<object?>>(() =>
                 {
-                    var list = new List<object>();
+                    var list = new List<object?>();
 
                     scopeProvider.ForEachScope((obj, state) => state.Add(obj), list);
 
@@ -37,6 +37,6 @@ namespace Microsoft.Extensions.Logging.Structured
         public object? Message { get; }
         public Exception? Exception { get; }
         public string RenderedMessage { get; }
-        public IEnumerable<object> Scope => _scope.Value;
+        public IEnumerable<object?> Scope => _scope.Value;
     }
 }
